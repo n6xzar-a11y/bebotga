a8e830b97d4300c0d68a226112cce8cce0b891fe
n6xzar-a11y <n6xzar@gmail.com>
2026-05-03 17:38:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ class MusicPlayer:
 
             source = discord.FFmpegPCMAudio(
                 audio_url,
+                executable="ffmpeg",
                 before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
                 options="-vn"
             )
